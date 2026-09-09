6f6bbeb16ead65438e11985f8408952fb1df2aae
Ahmad_EY <ahmadelyakubu@gmail.com>
2026-09-09 23:42:42 +0100

    Implement window positioning for Brave and Notepad
    Add PowerShell commands to position windows on secondary display

--- test_skill.py
+++ test_skill.py
@@ -14,3 +14,10 @@ def main():
 $displays = [System.Windows.Forms.Screen]::AllScreens
 $secondary = $displays[1].Bounds
 $halfWidth = [math]::Floor($secondary.Width / 2)
+$halfHeight = [math]::Floor($secondary.Height / 2)
+
+Start-Sleep -Seconds 1
+
+Start-Process -FilePath \"brave\" -WindowStyle Hidden | ForEach-Object {{ $_.MainWindowHandle = [System.IntPtr]::new($secondary.Left + $halfWidth, $secondary.Top + $halfHeight) }}
+
+Start-Process -FilePath \"notepad\" -WindowStyle Hidden | ForEach-Object {{ $_.MainWindowHandle = [System.IntPtr]::new($secondary.Left, $secondary.Top, $halfWidth, $halfHeight) }}
