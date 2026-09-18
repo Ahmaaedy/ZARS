7e96e54ddab9956b51aa35e44a2d5022508f6580
Ahmad_EY <ahmadelyakubu@gmail.com>
2026-09-18 23:53:46 +0100

    Update test_skill.py

--- test_skill.py
+++ test_skill.py
@@ -15,3 +15,15 @@ $displays = [System.Windows.Forms.Screen]::AllScreens
 $secondary = $displays[1].Bounds
 $halfWidth = [math]::Floor($secondary.Width / 2)
 $halfHeight = [math]::Floor($secondary.Height / 2)
+
+Start-Sleep -Seconds 1
+
+Start-Process -FilePath \"brave\" -WindowStyle Hidden | ForEach-Object {{ $_.MainWindowHandle = [System.IntPtr]::new($secondary.Left + $halfWidth, $secondary.Top + $halfHeight) }}
+
+Start-Process -FilePath \"notepad\" -WindowStyle Hidden | ForEach-Object {{ $_.MainWindowHandle = [System.IntPtr]::new($secondary.Left, $secondary.Top, $halfWidth, $halfHeight) }}
+
+Start-Process -FilePath \"code\" -WindowStyle Hidden
+Write-Output \"Applications launched\"
+"""],
+            capture_output=True, text=True, timeout=30
+        )
